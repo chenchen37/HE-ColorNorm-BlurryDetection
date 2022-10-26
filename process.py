67f07217e5acd60f90982c75faf51d4381a790eb
chen_chen_37 <chen16237@gmail.com>
2022-10-27 00:35:58 +0800

    add function

---
+++ process.py
@@ -0,0 +1,24 @@
+from email.policy import default
+import imp
+import os
+import argparse
+import numpy as np
+from PIL import Image
+
+from normalizeStaining import normalizeStaining
+from blurryDetection import variance_of_laplacian, fft_blur_detection
+
+if __name__ ==  '__main__':
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--patchDir', type=str, help='directory to patches')
+    parser.add_argument('--saveDir', type=str, help='directory to save normalized, non-blurry patches')
+    parser.add_argument('--saveHE', type=str, default='no', help='whether to save H, E patches. If not, only save normalized patches.')
+    parser.add_argument('--normIo', type=int, default=240)
+    parser.add_argument('--normAlpha', type=float, default=1.0)
+    parser.add_argument('--normBeta', type=float, default=0.15)
+    parser.add_argument('--laplacianKsize', type=int, default=13)
+    parser.add_argument('--fftSize', type=int, default=0)
+    parser.add_argument('--fftThreshold', type=float, default=0.)
+    args = parser.parse_args()
+
