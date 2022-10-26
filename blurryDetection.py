afe8b2b9dd510c21c221524e4ae5ed798e9f7dc9
chen_chen_37 <chen16237@gmail.com>
2022-10-26 20:18:13 +0800

    init

---
+++ blurryDetection.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+
+def variance_of_laplacian(image, ksize=13):
+    '''
+    Input: image array (gray scale)
+    Output: variance of Laplacian
+    '''
+    return cv2.Laplacian(image, cv2.CV_64F, ksize=ksize).var()
+
+def fft_blur_detection(image, size, thresh):
+    h, w = image.shape
+    cX, cY = int(w/2.), int(h/2.)
+    
+    fft = np.fft.fft2(image)
+    fftShift = np.fft.fftshift(fft)
+    
+    fftShift[cY - size:cY + size, cX - size:cX + size] = 0
+    fftShift = np.fft.ifftshift(fftShift)
+    recon = np.fft.ifft2(fftShift)
+    
+    magnitude = 20 * np.log(np.abs(recon))
+    mean = np.mean(magnitude)
+    
+    return mean, (mean<=thresh)
+
